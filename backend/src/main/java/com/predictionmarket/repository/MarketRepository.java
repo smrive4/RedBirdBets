03becb092b967c4af6c0aef3dab484abab7c84ac
Aaron Sihweil <ASihweil1219@gmail.com>
2026-03-16 14:00:41 -0500

    Added BCrypt Password Encryption

--- backend/src/main/java/com/predictionmarket/repository/MarketRepository.java
+++ backend/src/main/java/com/predictionmarket/repository/MarketRepository.java
@@ -2,6 +2,7 @@ package com.predictionmarket.repository;
 
 import com.predictionmarket.model.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
--- backend/src/main/java/com/predictionmarket/service/UserService.java
+++ backend/src/main/java/com/predictionmarket/service/UserService.java
@@ -3,6 +3,7 @@ package com.predictionmarket.service;
 import com.predictionmarket.model.User;
 import com.predictionmarket.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,19 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User registerUser(String username, String password) {
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
+    }
+
+    public boolean loginUser(String username, String password) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) return false;
+        return passwordEncoder.matches(password, user.getPassword());
     }
 }
