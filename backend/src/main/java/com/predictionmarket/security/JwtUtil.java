62d5a6fcf4352a654207c6e909a0659fd5f7ec35
smrive4 <193434163+smrive4@users.noreply.github.com>
2026-04-28 01:50:17 -0500

    JwtUtil Class

---
+++ backend/src/main/java/com/predictionmarket/security/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.predictionmarket.security;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class JwtUtil {
+    @Value("${app.jwt.secret}")
+    private String secretKey;  
+    private SecretKey key;  
+
+    @PostConstruct
+    public void init(){
+        byte[] keyBytes = Decoders.BASE64.decode(this.secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+    public String createToken(String username){
+        Instant now = Instant.now();
+        
+        return Jwts.builder()
+            .subject(username)
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(now.plus(Duration.ofHours(8))))
+            .signWith(key)
+            .compact();
+    }
+
+    public String getUsername(String token){
+        Claims claim = Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+
+        return claim.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+           return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
