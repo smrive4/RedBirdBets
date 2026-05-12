5debe3e48ddbd829e340ddfc53ea72e224b9a664
smrive4 <193434163+smrive4@users.noreply.github.com>
2026-05-11 21:59:12 -0500

    minor changes

--- backend/src/main/java/com/predictionmarket/security/SecurityConfig.java
+++ backend/src/main/java/com/predictionmarket/security/SecurityConfig.java
@@ -1,20 +1,19 @@
 package com.predictionmarket.security;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-
-
-import java.util.List;
 /* Configuration class for setting up security filters and CORS settings */
 @Configuration
 public class SecurityConfig {
@@ -47,7 +46,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
         "http://localhost:5173",
-        "http://3.14.80.111"
+        "http://3.14.80.111",
+        "https://red-bird-bets.vercel.app"
     ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
