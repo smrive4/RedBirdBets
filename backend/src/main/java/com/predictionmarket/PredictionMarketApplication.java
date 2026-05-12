36429917a17cd4a6bca9d30cca98b952f2c29ece
smrive4 <193434163+smrive4@users.noreply.github.com>
2026-05-11 20:33:10 -0500

    minor changes

--- backend/src/main/java/com/predictionmarket/PredictionMarketApplication.java
+++ backend/src/main/java/com/predictionmarket/PredictionMarketApplication.java
@@ -2,15 +2,12 @@ package com.predictionmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 /* Main application class for the Prediction Market */
 @SpringBootApplication
 public class PredictionMarketApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("./").load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(PredictionMarketApplication.class, args);
 	}
 }
